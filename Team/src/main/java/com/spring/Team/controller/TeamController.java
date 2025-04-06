57dbf74aaaf2e2020572af152ef46fc937e4b4f6
nada khalefa <NadaKhalefa721@gmail.com>
2025-04-07 01:39:37 +0200

    RestTemplate

--- Team/src/main/java/com/spring/Team/controller/TeamController.java
+++ Team/src/main/java/com/spring/Team/controller/TeamController.java
@@ -1,10 +1,15 @@
 package com.spring.Team.controller;
 
 import com.spring.Team.model.Team;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/team-players")
@@ -13,7 +18,20 @@ public class TeamController {
     @GetMapping("/team/{from}/player/{to}")
     public Team teamWithPlayer(@PathVariable String from,@PathVariable String to){
 
-        Team team= new Team(1,from, to, 100, "NONE","50");
+        Map<String, String> urlAttribute= new HashMap<>();
+        urlAttribute.put("from", from);
+        urlAttribute.put("to", to);
+        // request to another service
+        ResponseEntity<Team> teamResponseEntity= new RestTemplate().getForEntity(
+                "http://localhost:8888/football-players/buy/{from}/player/{to}",
+                Team.class,
+                urlAttribute
+        );
+
+        // response of teamEntity
+        Team teamResponse= teamResponseEntity.getBody();
+
+        Team team= new Team(teamResponse.getId(),teamResponse.getFrom(), teamResponse.getTo(), teamResponse.getMoneyTeam(), "NONE","50");
 
         return team;
     }
