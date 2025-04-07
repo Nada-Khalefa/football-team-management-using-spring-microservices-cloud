f50bb56b9b455ccde27d92a05f874bd32904fc5a
nada khalefa <NadaKhalefa721@gmail.com>
2025-04-07 03:46:27 +0200

    spring-cloud-starter-ribbon

--- Team/src/main/java/com/spring/Team/config/ApiCall.java
+++ Team/src/main/java/com/spring/Team/config/ApiCall.java
@@ -1,13 +1,16 @@
 package com.spring.Team.config;
 
 import com.spring.Team.model.Team;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 // name of service you need
 // url of same service
-@FeignClient(name = "football-players",url = "localhost:8888")
+//@FeignClient(name = "football-players",url = "localhost:8888")
+@FeignClient(name = "football-players")
+@RibbonClient(name = "football-players")
 public interface ApiCall {
 
     @GetMapping("/football-players/buy/{from}/player/{to}")
