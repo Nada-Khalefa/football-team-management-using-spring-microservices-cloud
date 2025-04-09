db97982efc6411ef3b6091132adef2ab2faba0e9
nada khalefa <NadaKhalefa721@gmail.com>
2025-04-09 15:13:02 +0200

    register microservices

--- Team/src/main/java/com/spring/Team/config/Proxy.java
+++ Team/src/main/java/com/spring/Team/config/Proxy.java
@@ -2,7 +2,7 @@ package com.spring.Team.config;
 
 import com.spring.Team.model.Team;
 //import org.springframework.cloud.netflix.ribbon.RibbonClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+//import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 // url of same service
 //@FeignClient(name = "football-players",url = "localhost:8589")
 @FeignClient(name = "football-players")
-@RibbonClient(name = "football-players")
+//@RibbonClient(name = "football-players")
 public interface Proxy {
 
     @GetMapping("/football-players/buy/{from}/player/{to}")
