fe2067af526fbbea239faea1a0ee24a237940ec3
nada khalefa <NadaKhalefa721@gmail.com>
2025-04-13 16:37:06 +0200

    resilience @retry

--- players-statistics/src/main/java/com/spring/players_statistics/PlayersStatisticsApplication.java
+++ players-statistics/src/main/java/com/spring/players_statistics/PlayersStatisticsApplication.java
@@ -2,8 +2,10 @@ package com.spring.players_statistics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
+//@EnableHystrix
 public class PlayersStatisticsApplication {
 
 	public static void main(String[] args) {
--- players-statistics/src/main/java/com/spring/players_statistics/controller/PlayersStatisticsController.java
+++ players-statistics/src/main/java/com/spring/players_statistics/controller/PlayersStatisticsController.java
@@ -1,16 +1,30 @@
 package com.spring.players_statistics.controller;
 
 
+//import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.spring.players_statistics.config.PlayersStatisticsConfiguration;
 import com.spring.players_statistics.model.PlayersStatistics;
+//import com.spring.players_statistics.service.SalaryService;
+//import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/players-management")
 public class PlayersStatisticsController {
+
+
+    private Logger logger=
+            LoggerFactory.getLogger(PlayersStatisticsController.class);
+
 
     @Autowired
     private PlayersStatisticsConfiguration playersStatisticsConfiguration;
@@ -21,4 +35,23 @@ public class PlayersStatisticsController {
 
       return new PlayersStatistics(playersStatisticsConfiguration.getNumberTeams(),playersStatisticsConfiguration.getNumberPlayers(),playersStatisticsConfiguration.getCountry());
     }
+
+    @GetMapping("/salary")
+    //@HystrixCommand(fallbackMethod = "callBack")
+    @Retry(name = "requestsTimes", fallbackMethod ="callBack" )
+    public Integer getSalary(){
+        logger.info("Request Done ...........");
+        ResponseEntity<Integer> responseEntity=
+                new RestTemplate().getForEntity(
+                        "http://localhost:4758/salary",
+                        Integer.class
+                );
+        return responseEntity.getBody();
+//        return 1000;
+    }
+
+    public Integer callBack(Exception e){
+        return 5000;
+    }
+
 }
